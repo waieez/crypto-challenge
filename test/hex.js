e46e07f1b33d80c5b2e8ad409984a8678e74d1a8
Eli Xian <elixian@uber.com>
2017-07-30 20:15:28 -0700

    Update hex to use new bin library

--- test/hex.js
+++ test/hex.js
@@ -6,12 +6,7 @@ logger.level = 'error';
 module.exports = (() => {
   test('hex.encode', t => {
     const description = 'Should be able to encode a hex string';
-    const cases = [
-      ['0', '30'],
-      ['1', '31'],
-      ['hello world', '68656c6c6f20776f726c64']
-    ];
-
+    const cases = [['0', '30'], ['1', '31'], ['hello world', '68656c6c6f20776f726c64']];
     cases.forEach(([input, expectation]) => {
       t.equals(hex.encode(input), expectation, description);
     });
@@ -20,7 +15,6 @@ module.exports = (() => {
   test('hex.decode', t => {
     const description = 'Should be able to decode a hex string';
     const cases = [['hello world', '68656c6c6f20776f726c64']];
-
     cases.forEach(([expectation, input]) => {
       t.equals(hex.decode(input), expectation, description);
     });
@@ -28,26 +22,10 @@ module.exports = (() => {
 
   test('hex.toBinary', t => {
     const description = 'Should be able to convert a value to binary';
-    const cases = [
-      ['0', '0000'],
-      ['1', '0001'],
-      ['2', '0010'],
-      ['3', '0011'],
-      ['4', '0100'],
-      ['5', '0101'],
-      ['6', '0110'],
-      ['7', '0111'],
-      ['8', '1000'],
-      ['9', '1001'],
-      ['a', '1010'],
-      ['b', '1011'],
-      ['c', '1100'],
-      ['d', '1101'],
-      ['e', '1110'],
-      ['f', '1111']
-    ];
-    cases.forEach(([input, expectation]) => {
-      t.equals(hex.toBinary(input), expectation, description);
+    const cases = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
+    cases.forEach((input, expectation) => {
+      const buffer = hex.toBinary(input);
+      t.equals(new Uint8Array(buffer)[0], expectation, description);
     });
   });
 })();
